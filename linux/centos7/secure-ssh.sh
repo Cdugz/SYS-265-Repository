79b3360eefe24deca1523b111ea3a3df447ad952
Cdugz <cameron.dugger@mymail.champlain.edu>
2026-02-15 19:57:27 -0500

    added web01 ssh key

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -3,4 +3,15 @@
 #creates a new ssh user using $1 parameter
 #adds a public key from the local repo or curled from the remote repo
 #removes roots ability to ssh in
-echo "ALL YOUR CODE GOES HERE"
+
+#!/bin/bash
+
+useradd -m testuser
+
+mkdir -p /home/testuser/.ssh
+cp !/.ssh/id_rsa.pub /home/testuser/.ssh/authorized_keys
+chmod 600 /home/testuser/.ssh/authorized_keys
+chmod 700 /home/testuser/.ssh
+chown -R testuser:testuser /home/testuser/.ssh
+
+
