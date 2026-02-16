be6bf077677e62471b3702ca7b17f9f6e3d5fb60
Cameron <cameron.dugger@mymail.champlain.edu>
2026-02-15 20:34:13 -0500

    Update secure-ssh.sh

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -9,7 +9,7 @@
 useradd -m testuser
 
 mkdir -p /home/testuser/.ssh
-cp !/.ssh/id_rsa.pub /home/testuser/.ssh/authorized_keys
+cp ../public-keys/id_rsa.pub /home/testuser/.ssh/authorized_keys
 chmod 600 /home/testuser/.ssh/authorized_keys
 chmod 700 /home/testuser/.ssh
 chown -R testuser:testuser /home/testuser/.ssh
